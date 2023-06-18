64ed0509ef4f28e03bc2f3d9929ff6b534e07957
Abdullajon <110884479+AbdullajonOdilov@users.noreply.github.com>
2023-06-18 21:13:18 +0500

    login, logout added

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login
-
+from django.contrib.auth import logout
 from rest_framework import status, generics
 
 from rest_framework.views import APIView
