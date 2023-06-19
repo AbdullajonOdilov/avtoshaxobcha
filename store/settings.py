ed842cb3be2e5a7ed2b375060f2e278358cba36f
Abdullajon <110884479+AbdullajonOdilov@users.noreply.github.com>
2023-06-19 15:38:37 +0500

    allowed hosts changed

--- store/settings.py
+++ store/settings.py
@@ -43,15 +43,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
+
 
 ROOT_URLCONF = 'store.urls'
 
